043ce517aeadcabbb2441ec9958fd2d31681ee73
kapibaba <jksuan@163.com>
2025-02-04 11:45:18 +0800

    Add youtube api key and modify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com"],
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
